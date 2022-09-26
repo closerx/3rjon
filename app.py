daa93d41bdeed1af57194f9b2acdc91c4bf79571
Hadi Hakami <74460140+closerx@users.noreply.github.com>
2022-09-26 08:12:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__,template_folder="templates")
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    #return render_template("index.html")
+    return "Hello world"
     
     
