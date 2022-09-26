8de5ef7304664f131141d2163994cc830a4470f7
Hadi Hakami <74460140+closerx@users.noreply.github.com>
2022-09-26 12:01:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template
 
 app = Flask(__name__,template_folder="./Templates")
-
+def get_data(city):
+	url="http://api.aladhan.com/v1/timingsByCity?city="+city+"&country=Kingdom%20Saudia%20Arabia&method=4"
+	response=requests.get(url).json()
+	hijri=response["data"] ["date"]["hijri"]["date"]
+	mladi=response["data"]["date"]["gregorian"]["date"]
+	times=response["data"]["timings"]
+	month=response["data"]["date"]["hijri"]["month"]["ar"]
+	day=response["data"]["date"]["hijri"]["weekday"]["ar"]
+	data={
+	"times":times,
+	"hijri":hijri,
+	"mladi":mladi,
+	"month":month,
+	"day":day
+	}
+	return data
 @app.route('/')
 def index():
     return render_template("index.html")
     
-    
+@app.route('/data/<name>')
+def my_view_func(name):
+    return get_data(name)   
 if __name__ == "__main__":
 	app.run()
